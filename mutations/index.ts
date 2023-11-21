153a711922d3f7efea2e18a42db45c241f04e3d2
Srijan Srivastava <srijan@proactiveforher.com>
2023-11-22 02:21:31 +0530

    FEAT: Get Price Range Query

--- mutations/index.ts
+++ mutations/index.ts
@@ -3,6 +3,39 @@ import type { Context } from '.keystone/types';
 
 export const extendGraphqlSchema = graphql.extend((base) => {
   return {
+    query: {
+      // Define a new query in your GraphQL API for getting price range
+      getPriceRange: graphql.field({
+        type: graphql.object<{ min: number; max: number }>()({
+          name: 'PriceRange',
+          fields: {
+            min: graphql.field({ type: graphql.Float }),
+            max: graphql.field({ type: graphql.Float }),
+          },
+        }),
+        async resolve(_, __, context: Context) {
+          // Use Prisma's aggregation capabilities to get the min and max price
+          const priceAggregate = await context.prisma.product.aggregate({
+            _min: {
+              price: true,
+            },
+            _max: {
+              price: true,
+            },
+          });
+          const min = priceAggregate._min.price;
+          const max = priceAggregate._max.price;
+          if (!min || !max)
+            throw new Error('Cannot aggregate a minimum/maximum price');
+
+          return {
+            min,
+            max,
+          };
+        },
+        // Return the price range
+      }),
+    },
     mutation: {
       addToCart: graphql.field({
         type: base.object('CartItem'),
